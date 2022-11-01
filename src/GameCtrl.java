312141730505d78973f75f48a7e56344ea3a701e
kotteletfisk <ostekugle@gmail.com>
2022-11-01 11:00:56 +0100

    Merged NewFeature

--- src/GameCtrl.java
+++ src/GameCtrl.java
@@ -76,9 +76,16 @@ public class GameCtrl
 
 	public static void main(String[] args)
 	{
+		MainFeature mn = new MainFeature();
+		NewFeature nw = new NewFeature();
+
+		System.out.println(mn.toString());
+		nw.method();
+
 		IThinker thinker = chooseType("Thinker") == 1 ? new HumanThinker() : new ComputerThinker();
 		IGuesser guesser = chooseType("Guesser") == 1 ? new HumanGuesser() : new ComputerGuesser();
 		GameCtrl gc = new GameCtrl(0, 100);
 		gc.runGame(thinker, guesser);
+
 	}
 }
