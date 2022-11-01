b7f1260f73ad1338fdc438d009f3a885c93d9499
kotteletfisk <ostekugle@gmail.com>
2022-11-01 11:15:53 +0100

    Added AlienThinker.java

---
+++ src/AlienThinker.java
@@ -0,0 +1,32 @@
+public class AlienThinker implements IThinker
+{
+    @Override
+    public void yourTurn()
+    {
+
+    }
+
+    @Override
+    public String getName()
+    {
+        return null;
+    }
+
+    @Override
+    public void newGame(int min, int max, String opponent, String type)
+    {
+
+    }
+
+    @Override
+    public Answer evaluateGuess(int guess, String opponent)
+    {
+        return null;
+    }
+
+    @Override
+    public boolean endOfGame(int numberOfGuesses, String opponent)
+    {
+        return false;
+    }
+}
